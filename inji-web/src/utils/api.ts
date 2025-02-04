95e6cf0c3770db0d785aa5d5305f932b9435cbdb
Sk Mijan Hossain <skmijanhossain@gmail.com>
2025-02-04 23:10:19 +0530

    remove env

--- inji-web/src/utils/api.ts
+++ inji-web/src/utils/api.ts
@@ -13,8 +13,8 @@ export enum MethodType {
 
 export class api {
 
-    static mimotoHost = "http://localhost:3010";
-    //  static mimotoHost = window.location.origin + "/v1/mimoto";
+    // static mimotoHost = "http://localhost:3010";
+     static mimotoHost = window.location.origin + "/v1/mimoto";
 
     static authorizationRedirectionUrl = window.location.origin + "/redirect";
 
